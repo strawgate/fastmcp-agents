f4a7612d320a73b54a55d0cb200cd27e334fcd52
William Easton <bill.easton@elastic.co>
2025-08-04 11:36:21 -0500

    Ensure linting/type checking happens in the working dir

--- fastmcp-agents-bridge/fastmcp_agents_bridge_pydantic_ai/tests/test_bridge.py
+++ fastmcp-agents-bridge/fastmcp_agents_bridge_pydantic_ai/tests/test_bridge.py
@@ -1,15 +1,13 @@
-from pathlib import Path
 from typing import TYPE_CHECKING
 
 import pytest
 from fastmcp import FastMCP
 from fastmcp.mcp_config import MCPConfig, TransformingStdioMCPServer
-from pydantic_ai import Agent, RunContext
+from pydantic_ai import Agent
 from pydantic_ai.models.google import GoogleModel
 from pydantic_ai.providers.google import GoogleProvider
-from pydantic_ai.toolsets import AbstractToolset
 
-from fastmcp_agents.bridge.pydantic_ai.toolset import DynamicToolset, FastMCPServerToolset
+from fastmcp_agents.bridge.pydantic_ai.toolset import FastMCPServerToolset
 
 if TYPE_CHECKING:
     from fastmcp.server.proxy import FastMCPProxy
@@ -59,41 +57,3 @@ async def test_agent_with_bridge(model: GoogleModel):
 
     result = await agent.run("What tools do you have available? Please test all of the tools to make sure they work.")
     print(result.output)
-
-
-# async def test_agent_with_bridge_customize(model: GoogleModel):
-#     mcp_config = MCPConfig(
-#         mcpServers={
-#             "echo": TransformingStdioMCPServer(
-#                 command="uvx",
-#                 args=["mcp-server-time"],
-#                 tools={},
-#             ),
-#         },
-#     )
-
-#     async def prepare_mcp_config(ctx: RunContext[Path]) -> AbstractToolset[Path]:
-#         return FastMCPServerToolset[Path].from_mcp_config(mcp_config=mcp_config)
-
-#     dynamic_toolset: DynamicToolset[Path] = DynamicToolset[Path](build_toolset_fn=prepare_mcp_config)
-
-#     agent = Agent[Path, str](
-#         model,
-#         system_prompt="Be concise, reply with one sentence.",
-#         toolsets=[dynamic_toolset],
-#         deps_type=Path,
-#         output_type=str,
-#     )
-
-#     async with agent:
-#         result = await agent.run(
-#             deps=Path(), user_prompt="What tools do you have available? Please test all of the tools to make sure they work."
-#         )
-#         print(result.output)
-
-#     async with agent:
-#         result = await agent.run(
-#             deps=Path(), user_prompt="What tools do you have available? Please test all of the tools to make sure they work."
-#         )
-#         print(result.output)
-#     print(dynamic_toolset.toolset.toolsets)
